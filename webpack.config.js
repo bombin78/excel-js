5c251ab84a3ccef10748c2335ecf62a5a61d509d
bombin78 <bombin78@mail.ru>
2022-01-12 23:57:52 +0300

    Finish project config

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,94 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: {
-        main: './index.js',
-    },
-    resolve: {
-        extension: ['.js'],
-        alias: {
-            '@': path.resolve(__dirname, 'src'),
-            '@core': path.resolve(__dirname, 'src', 'core'),
+module.exports = (env, argv) => {
+    const isProd = argv.mode === 'production';
+    const isDev = !isProd;
+
+    const getFileName = (ext) => isProd
+        ? `[name].[contenthash].bundle.${ext}`
+        : `[name].bundle.${ext}`;
+
+    const getPlugins = () => {
+        const base = [
+            new HtmlWebpackPlugin({
+                template: './index.html',
+            }),
+            new CopyPlugin({
+                patterns: [
+                    {
+                        from: path.resolve(__dirname, 'src', 'favicon.ico'),
+                        to: path.resolve(__dirname, 'dist'),
+                    },
+                ],
+            }),
+            new MiniCssExtractPlugin({
+                filename: getFileName('css'),
+            }),
+        ];
+
+        if (isDev) {
+            base.push(new ESLintPlugin());
+        }
+
+        return base;
+    };
+
+    return {
+        // Возможно target уже не требуется
+        // target: 'web'
+        context: path.resolve(__dirname, 'src'),
+        entry: {
+            main: [
+                'core-js/stable',
+                'regenerator-runtime/runtime',
+                './index.js',
+            ],
         },
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name].bundle.js',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './index.html',
-        }),
-        new CopyPlugin({
-            patterns: [
-                { 
-                    from: path.resolve(__dirname, 'src', 'favicon.ico'), 
-                    to: path.resolve(__dirname, 'dist') 
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: getFileName('js'),
+            clean: true,
+        },
+        resolve: {
+            extensions: ['.js'],
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@core': path.resolve(__dirname, 'src', 'core'),
+            },
+        },
+        devServer: {
+            port: 3000,
+            open: true,
+            hot: true,
+            watchFiles: './',
+        },
+        devtool: isDev ? 'source-map' : false,
+        plugins: getPlugins(),
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        'sass-loader',
+                    ],
+                },
+                {
+                    test: /\.m?js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['@babel/preset-env'],
+                        },
+                    },
                 },
             ],
-          }),
-    ],
-}
+        },
+    };
+};
